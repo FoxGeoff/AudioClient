18f7c4d280b19db96d665b934cc9804abfa0600e
FoxGeoff <fox.geoffrey@gmail.com>
2018-10-10 18:04:10 -0700

    Add test data

--- src/app/shared/in-memory-data.service.ts
+++ src/app/shared/in-memory-data.service.ts
@@ -4,9 +4,16 @@ import { TestValue } from '../interface/test-value';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const testValues = [
-      { Id: 1, Value: 'Value1', Description: 'Questions about a nice hero' },
-      { Id: 2, Value: 'Value2', Description: 'Questions about a drug hero' },
-      { Id: 3, Value: 'Value3', Description: 'Questions about a braging hero' }
+      { Id: 11, Value: 'Value1', Description: 'Questions about a nice hero' },
+      { Id: 12, Value: 'Value2', Description: 'Questions about a drug hero' },
+      { Id: 13, Value: 'Value3', Description: 'Questions about a braging hero' },
+      { Id: 14, Value: 'Value4', Description: 'Questions about a nice hero' },
+      { Id: 15, Value: 'Value5', Description: 'Questions about a drug hero' },
+      { Id: 16, Value: 'Value6', Description: 'Questions about a braging hero' },
+      { Id: 17, Value: 'Value7', Description: 'Questions about a nice hero' },
+      { Id: 18, Value: 'Value8', Description: 'Questions about a drug hero' },
+      { Id: 19, Value: 'Value9', Description: 'Questions about a braging hero' },
+      { Id: 20, Value: 'Value10', Description: 'Questions about a braging hero' }
     ];
     return { testValues };
   } 
